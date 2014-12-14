aac78f86028908bfc4f777e0471f8873a22770d9
Simon Winkler <sw@fastseeders.com>
2014-12-14 23:46:41 +0100

    add 'get' method

--- index.js
+++ index.js
@@ -4,18 +4,24 @@ var $ = require("jquery"),
 	_ = require("lodash"),
 	
 	divgrid = function (config) {
+
+		// config.size
+		// config.el
+		// config.$target
 		
-		var create_el = function () {
+		var create_str = function () {
 				return "<" + config.el + ">" + "</" + config.el + ">";
 			},
-			grid_str = _.times(config.length * config.length, create_el).join("");
+			grid_str = _.times(config.length * config.length, create_str).join("");
 
+		config.$target.append(grid_str);
+
+		return {
+			get: function (x, y) {
+				return $(config.$target.children()[x + (y * 10)]);
+			}
+		}
 
 	};
-
-	divgrid({
-		length: 2,
-		el: "div"
-	});
 
 module.exports = divgrid;
