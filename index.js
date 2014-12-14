d94bc024f9c3709b25109fc5ae0228901688f710
Simon Winkler <sw@fastseeders.com>
2014-12-15 00:03:54 +0100

    set static size of grid cells

--- index.js
+++ index.js
@@ -12,9 +12,18 @@ var $ = require("jquery"),
 		var create_str = function () {
 				return "<" + config.el + ">" + "</" + config.el + ">";
 			},
-			grid_str = _.times(config.length * config.length, create_str).join("");
+			grid_str = _.times(config.length * config.length, create_str).join(""),
+			el_size = config.$target.width() / config.length;
 
-		config.$target.append(grid_str);
+		config
+			.$target.append(grid_str);
+		config
+			.$target
+			.children(config.el)
+			.css({
+				"width": size,
+				"height": size
+			});
 
 		return {
 			get: function (x, y) {
